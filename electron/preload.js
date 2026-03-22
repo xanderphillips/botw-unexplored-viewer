ab5adac4088a03ade00f8da508efe9b32ba5285a
pphil <pphil@example.com>
2026-03-21 23:18:55 -0400

    feat(electron): add contextBridge preload for setup window

---
+++ electron/preload.js
@@ -0,0 +1,8 @@
+'use strict';
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+    pickFolder:  ()    => ipcRenderer.invoke('pick-folder'),
+    saveConfig:  (cfg) => ipcRenderer.invoke('save-config', cfg),
+    getConfig:   ()    => ipcRenderer.invoke('get-config'),
+});
